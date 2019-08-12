329576ebca17acbde2625e36fa4b45c181c3bb52
dhrandy <dhrandy@gmail.com>
2019-08-11 20:39:44 -0400

    change photo folders, added second car and updated

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,3 +1,5 @@
+//Chevell Slideshow
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -13,6 +15,36 @@ function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("demo");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+//Truck Slideshow
+
+var slideIndex = 1;
+showSlides1(slideIndex); //Change showSlides #
+
+function plusSlides(n) {
+    showSlides1(slideIndex += n);
+}
+
+function currentSlide1(n) {
+    showSlides1(slideIndex = n);
+}
+
+function showSlides1(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides-1"); //Change mySlides # and change in html
+    var dots = document.getElementsByClassName("demo-1"); //Change demo # and change in html
     if (n > slides.length) { slideIndex = 1 }
     if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
